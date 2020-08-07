d41ec452b36164f6c86d2c031418f29426d0d024
Alex Mata <alexmata92@ymail.com>
2020-08-07 15:46:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,12 @@
 def reverse_each_word(string)
   array = string.split
   reverse_array = []
+  reverse_string = ""
   array.each do |element|
     reverse_array << element.reverse
   end
-  reverse_array.collect 
+  reverse_array.collect do |element|
+    element << reverse_string
+  end
+  reverse_string
 end 
